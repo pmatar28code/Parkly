d257b595046ad6fe024130691d96f640645fba31
pmatar28code <78227811+pmatar28code@users.noreply.github.com>
2021-04-19 17:55:30 -0500

    MapView Working
    gradle properties added the key
    project gradel all repositories added Maven/map
    module gradle, implementations

--- app/src/main/java/com/example/parkly/MainActivity.kt
+++ app/src/main/java/com/example/parkly/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.parkly
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mapbox.mapboxsdk.maps.MapView
 
 class MainActivity : AppCompatActivity() {
+var test : MapView?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
