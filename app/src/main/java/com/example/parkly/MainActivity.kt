3bb8c934e48a3d6cda81cac0f7728e17ce1f7397
pmatar28code <78227811+pmatar28code@users.noreply.github.com>
2021-04-22 08:25:42 -0500

    app working with walking route to parked car form current location, updated implementation for services, video is not up to date

--- app/src/main/java/com/example/parkly/MainActivity.kt
+++ app/src/main/java/com/example/parkly/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.example.parkly
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import android.location.Location
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.graphics.drawable.toBitmap
 import com.example.parkly.databinding.ActivityMainBinding
 import com.mapbox.android.core.permissions.PermissionsListener
 import com.mapbox.android.core.permissions.PermissionsManager
+import com.mapbox.api.directions.v5.DirectionsCriteria
+import com.mapbox.api.directions.v5.MapboxDirections
+import com.mapbox.api.directions.v5.models.DirectionsResponse
+import com.mapbox.core.constants.Constants
+import com.mapbox.geojson.LineString
 import com.mapbox.geojson.Point
 import com.mapbox.mapboxsdk.Mapbox
 import com.mapbox.mapboxsdk.geometry.LatLng
@@ -19,17 +26,24 @@ import com.mapbox.mapboxsdk.location.modes.RenderMode
 import com.mapbox.mapboxsdk.maps.MapView
 import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.Style
+import com.mapbox.mapboxsdk.style.layers.LineLayer
+import com.mapbox.mapboxsdk.style.layers.Property
 import com.mapbox.mapboxsdk.style.layers.PropertyFactory
 import com.mapbox.mapboxsdk.style.layers.SymbolLayer
 import com.mapbox.mapboxsdk.style.sources.GeoJsonSource
 import com.mapbox.mapboxsdk.style.sources.Source
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity(),PermissionsListener {
     companion object{
-       private const val MAPBOX_KEY = "pk.eyJ1IjoicG1hdGFyMjhjb2RlIiwiYSI6ImNrbnJ4anpzYTBuMzkyb3Bob3lwNjI3bTcifQ.1vv5YfZsK6KtKLd_cG7CQw"
-       private const val PIN_IMAGE = "PIN_IMAGE"
-       private const val CAR_LOCATION_SOURCE = "CAR_LOCATION_SOURCE"
-       private const val CAR_LOCATION_SYMBOL = "CAR_LOCATION_SYMBOL"
+        private const val MAPBOX_KEY = "pk.eyJ1IjoicG1hdGFyMjhjb2RlIiwiYSI6ImNrbnJ4anpzYTBuMzkyb3Bob3lwNjI3bTcifQ.1vv5YfZsK6KtKLd_cG7CQw"
+        private const val PIN_IMAGE = "PIN_IMAGE"
+        private const val CAR_LOCATION_SOURCE = "CAR_LOCATION_SOURCE"
+        private const val CAR_LOCATION_SYMBOL = "CAR_LOCATION_SYMBOL"
+        private const val ROUTE_SOURCE = "ROUTE_SOURCE"
+        private const val ROUTE_LAYER = "ROUTE_LAYER"
     }
     private var mapView:MapView ? =null
     private var map: MapboxMap ? =null
@@ -59,6 +73,21 @@ class MainActivity : AppCompatActivity(),PermissionsListener {
                     }
                     true
                 }
+
+                val routeLayer = LineLayer(ROUTE_LAYER, ROUTE_SOURCE).apply {
+                    setProperties(
+                        PropertyFactory.lineCap(Property.LINE_CAP_ROUND),
+                        PropertyFactory.lineJoin(Property.LINE_JOIN_ROUND),
+                        PropertyFactory.lineWidth(5f),
+                        PropertyFactory.lineColor(Color.BLUE)
+
+                    )
+                }
+                
+                map?.getStyle { style ->
+                    style.addSource(GeoJsonSource(ROUTE_SOURCE))
+                    style.addLayer(routeLayer)
+                }
             }
         }
 
@@ -74,6 +103,10 @@ class MainActivity : AppCompatActivity(),PermissionsListener {
                 var image = addParkedCar(it,point)
                 binding.carFav.setImageResource(image)
             }
+        }
+
+        binding.directionsFab.setOnClickListener {
+            getDirections()
         }
     }
 
@@ -105,6 +138,55 @@ class MainActivity : AppCompatActivity(),PermissionsListener {
                 .setNegativeButton(android.R.string.cancel,null)
     }
 
+    private fun getDirections(){
+        val lastKnownLocation = map?.locationComponent?.lastKnownLocation ?: return
+        if(parkedCarLocation == null){
+            return
+        }
+        val origin = Point.fromLngLat(lastKnownLocation.longitude,lastKnownLocation.latitude)
+        val destination = parkedCarLocation!!
+
+        val client = MapboxDirections.builder()
+            .origin(origin)
+            .destination(destination)
+            .accessToken(MAPBOX_KEY)
+            .overview(DirectionsCriteria.OVERVIEW_SIMPLIFIED)
+            .profile(DirectionsCriteria.PROFILE_WALKING)
+            .build()
+
+        client.enqueueCall(object : Callback<DirectionsResponse>{
+            override fun onFailure(call: Call<DirectionsResponse>, t: Throwable) {
+                Log.e("ERROR NO DIRECTIONS", " $t")
+            }
+
+            override fun onResponse(
+                call: Call<DirectionsResponse>,
+                response: Response<DirectionsResponse>
+            ) {
+                if(response.body() == null || response.body()!!.routes().size < 1){
+                    Log.e("Error","No routes found")
+                }else{
+
+                    handleDirectionsResponse(response.body() as DirectionsResponse)
+                }
+            }
+
+        })
+
+    }
+
+    private fun handleDirectionsResponse(response : DirectionsResponse){
+        val currentRoute = response.routes()[0]
+        map?.getStyle {style ->
+            val geometry = currentRoute.geometry() ?: return@getStyle
+            val source = style.getSourceAs<GeoJsonSource>(ROUTE_SOURCE)
+            source?.setGeoJson(LineString.fromPolyline(geometry,Constants.PRECISION_6))
+            style.getLayerAs<LineLayer>(ROUTE_LAYER)?.setProperties(
+                PropertyFactory.visibility(Property.VISIBLE)
+            )
+        }
+    }
+
     fun addParkedCar(style:Style, latLng: Point):Int{
         enableUserLocation(style)
         return if(parkedCarLocation == null) {
@@ -123,6 +205,10 @@ class MainActivity : AppCompatActivity(),PermissionsListener {
             style.apply {
                 removeLayer(CAR_LOCATION_SYMBOL)
                 removeSource(CAR_LOCATION_SOURCE)
+
+                getLayerAs<LineLayer>(ROUTE_LAYER)?.setProperties(
+                PropertyFactory.visibility(Property.NONE)
+                )
             }
             parkedCarLocation = null
             R.drawable.park_car
